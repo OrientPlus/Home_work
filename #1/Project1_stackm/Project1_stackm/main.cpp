9c2517e5fe83139691dd744e5226ddbffc8fcb67
DESKTOP-S24B0M7\gutro <gutrovr@gmail.com>
2022-09-10 22:19:26 +0500

    Tasks 1-3 have been implemented

---
+++ #1/Project1_stackm/Project1_stackm/main.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <array>
+#include <random>
+#include <stack>
+
+#include "stack.h"
+
+
+void gen_str(string *box, const int len, const int amount_str);
+
+string get_rand_str(size_t str_len);
+
+void My_stack();
+void Def_stack();
+
+int main()
+{
+	
+	My_stack();
+	Def_stack();
+
+	stackm<string> st1, st2;
+	st1.fill(STR_COUNT, STR_SIZE);
+	st2.fill(STR_COUNT, STR_SIZE);
+
+	if (st1.overlap(st2))
+		cout << "\nThe stacks are completely the same!" << endl;
+	else
+		cout << "The stacks don't match!" << endl;
+
+	system("pause");
+	return 0;
+} 
+
+void Def_stack()
+{
+	stack<string> st;
+	string* box = new string[STR_COUNT], str;
+
+	gen_str(box, STR_COUNT, STR_SIZE);
+
+	auto start = high_resolution_clock::now();
+
+	cout << "\n######################## DEFAULT STACK ######################" << endl;
+	for (int i = 0; i < STR_COUNT; i++)
+	{
+		st.push(box[i]);
+		cout << st.top() << endl;
+	}
+
+	cout << "\n---------------Strings are temporary variables-------------" << endl;
+	for (int i = 0; i < STR_COUNT; i++)
+	{
+		str = get_rand_str(STR_SIZE);
+		st.push(str);
+		cout << st.top() << endl;
+	}
+	
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	cout << "\n\n### TIME EXECUTABLE DEFAULT STACK: " << duration << " microseconds" << endl;
+
+	cout << "Stack size: " << st.size() << endl;
+	delete[] box;
+}
+
+void My_stack()
+{
+	stackm<string> my_stack;
+
+	cout << "\n########################### MY STACK ###########################" << endl;
+
+	string* box = new string[STR_COUNT];
+	gen_str(box, STR_COUNT, STR_SIZE);
+	my_stack.fill(box, STR_COUNT);
+	for (int i = 0; i < STR_COUNT; i++)
+	{
+		cout << my_stack.top(i) << endl;
+	}
+
+	cout << "\n---------------Strings are temporary variables-------------" << endl;
+
+	my_stack.fill(STR_COUNT, STR_SIZE);
+	for (int i = 0; i < STR_COUNT; i++)
+	{
+		cout << my_stack.top(i) << endl;
+	}
+
+	cout << "\n\n### TIME EXECUTABLE MY_STACK: " << my_stack.exec_time << " microseconds" << endl;
+	cout << "Stack size: " << my_stack.size() << endl;
+	delete[] box;
+}
+
+void gen_str(string* box, const int amount_str, const int len)
+{
+	string alph = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#";
+	int pos;
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dist(0, 53);
+
+	for (int i = 0; i < amount_str; i++)
+	{
+		for (int j = 0; j < len; j++)
+		{
+			pos = dist(gen);
+			box[i] += alph[pos];
+		}
+	}
+}
+
+string get_rand_str(size_t str_len)
+{
+	string alph = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#", result;
+	int pos;
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dist(0, 53);
+
+	for (int i = 0; i < str_len; i++)
+	{
+		pos = dist(gen);
+		result += alph[pos];
+	}
+	return result;
+}
---
+++ #1/Project1_stackm/Project1_stackm/stack.h
@@ -0,0 +1,223 @@
+#pragma once
+
+#include <iostream>
+#include <iterator>
+#include <random>
+#include <chrono>
+
+#define STR_SIZE 5
+#define STR_COUNT 5
+
+using namespace std;
+using namespace std::chrono;
+
+
+template <typename T>
+class stackm
+{
+	T* data;
+	size_t count, capacity, fill_count;
+
+	void resize_if_needed();
+public:
+	stackm(size_t capacity = 1'000);
+	~stackm();
+	void push(const T& elem);
+	void push(T&& elem);
+	void pop();
+	int overlap(stackm& st_two);
+	int subset(stackm& st_two);
+	const T& top() const;
+	T& top();
+	T& top(int N);
+	size_t size() const;
+	chrono::microseconds exec_time;
+
+	//Fills the stack with the contents of the container that is passed as an argument
+	void fill(const T* box, const int size_of_container);
+
+	//Fills the stack with randomly generated strings
+	void fill(const int random_amount, const int str_len);
+
+};
+
+template<typename T>
+inline void stackm<T>::resize_if_needed()
+{
+	if (this->fill_count + this->count >= this->capacity)
+	{
+		size_t new_capacity = this->fill_count + this->count + 10;
+		T* new_data = new T[new_capacity];
+		for (size_t i = 0; i < this->capacity; ++i)
+		{
+			new_data[i] = std::move(this->data[i]);
+		}
+		delete[] this->data;
+		this->capacity = new_capacity;
+		this->data = new_data;
+	}
+	if (this->count == this->capacity)
+	{
+		size_t new_capacity = capacity * 2;
+		T* new_data = new T[new_capacity];
+		for (size_t i = 0; i < this->capacity; ++i)
+		{
+			new_data[i] = std::move(this->data[i]);
+		}
+		delete[] this->data;
+		this->capacity = new_capacity;
+		this->data = new_data;
+	}
+}
+
+template<typename T>
+inline stackm<T>::stackm(size_t capacity)
+{
+	this->data = new T[capacity];
+	this->capacity = capacity;
+	this->count = 0;
+}
+
+template<typename T>
+inline stackm<T>::~stackm()
+{
+	delete[] this->data;
+}
+
+template<typename T>
+inline void stackm<T>::push(const T& elem)
+{
+	resize_if_needed();
+	this->data[count++] = elem;
+}
+
+template<typename T>
+inline void stackm<T>::push(T&& elem)
+{
+	cout << "Stole the pointer" <<endl;
+	resize_if_needed();
+	this->data[count++] = elem;
+}
+
+template<typename T>
+inline void stackm<T>::pop()
+{
+	if (this->count > 0)
+		--this->count;
+}
+
+template<typename T>
+inline int stackm<T>::overlap(stackm& st_two)
+{
+	T temp;
+	int iterator = 0;
+	if (this->size() != st_two.size())
+		return 0;
+	
+	for (int i=0; i<STR_COUNT; i++)
+	{
+		iterator--;
+		if (iterator == -1)
+			break;
+		temp = this->top(iterator);
+		if (this->top(iterator) == st_two.top(iterator))
+			continue;
+		else
+			return 0;
+	}
+	return 1;
+}
+
+template<typename T>
+inline int stackm<T>::subset(stackm& st_two)
+{
+	if (this->size() > st_two.size())
+		return 0;
+	
+
+}
+
+template<typename T>
+inline const T& stackm<T>::top() const
+{
+	return this->data[this->count - 1];
+}
+
+template<typename T>
+inline T& stackm<T>::top()
+{
+	return this->data[this->count - 1];
+}
+
+template<typename T>
+inline T& stackm<T>::top(int N)
+{
+	int temp_count = this->count;
+	for (int i = 0; i < N; i++)
+	{
+		if (temp_count > 0)
+		{
+			temp_count--;
+		}
+		else
+		{
+			cout << endl << "ERROR! Stack bottom reached earlier than expected!";
+			system("pause");
+			exit(-1);
+		}
+	}
+
+	return this->data[temp_count - 1];
+}
+
+template<typename T>
+inline size_t stackm<T>::size() const
+{
+	return this->count;
+}
+
+
+template<typename T>
+inline void stackm<T>::fill(const int rand_amount, const int str_len) 
+{
+	auto start = chrono::high_resolution_clock::now();
+
+	int pos = 0;
+	fill_count = rand_amount;
+	resize_if_needed();
+	string temp, alph = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#";
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dist(0, 53);
+	
+	for (int i = 0; i < rand_amount; i++)
+	{
+		temp.clear();
+		for (int j = 0; j < str_len; j++)
+		{
+			pos = dist(gen);
+			temp+= alph[pos];
+		}
+		this->data[count++] = temp;
+	}
+
+	auto stop = chrono::high_resolution_clock::now();
+	exec_time = duration_cast<microseconds>(stop - start);
+}
+
+template<typename T>
+inline void stackm<T>::fill(const T* box, const int size_of_mem)
+{
+	auto start = chrono::high_resolution_clock::now();
+
+	fill_count = size_of_mem;
+	resize_if_needed();
+	for (int i=0; i<size_of_mem; i++)
+	{
+		this->data[count++] = box[i];
+	}
+
+	auto stop = chrono::high_resolution_clock::now();
+	exec_time = duration_cast<microseconds>(stop - start);
+} 
+
